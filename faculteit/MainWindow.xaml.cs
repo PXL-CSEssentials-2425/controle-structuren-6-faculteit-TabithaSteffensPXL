35944f9d5e3e0ea504d0eead34d81b67cdf240f1
Tabitha Steffens <tabitha.steffens@student.pxl.be>
2024-10-17 20:16:53 +0200

    volledig gemaakt via een for loop

---
+++ faculteit/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace faculteit
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void closeButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void clearButton_Click(object sender, RoutedEventArgs e)
+        {
+            numberTextBox.Clear();
+            resultTextBox.Clear();
+            numberTextBox.Focus();
+        }
+
+        private void calculateButton_Click(object sender, RoutedEventArgs e)
+        {
+            int.TryParse(numberTextBox.Text, out int number);
+            int answer = number;
+           
+            if (number == 0)
+            {
+                resultTextBox.Text = 1.ToString();
+            } else
+            {
+                for (int i = number; i >= 1; i--)
+                {
+                    answer *= i;
+                }
+                resultTextBox.Text = answer.ToString();
+            }
+        }
+    }
+}
